28c2e42a7d6084ed8424371d8b630c8b6d218549
Colin Breakell <cbreakell99@gmail.com>
2024-10-26 15:17:21 +0000

    Bug Fix: Test for email bug fix

--- paradise/settings.py
+++ paradise/settings.py
@@ -111,7 +111,6 @@ AUTHENTICATION_BACKENDS = (
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
@@ -226,7 +225,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'paradisepending@example.com'
+    DEFAULT_FROM_EMAIL = 'cbreakell99@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
