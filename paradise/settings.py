3927cf25f417b4e9ab3042bdbef843ecd80df36b
Colin Breakell <cbreakell99@gmail.com>
2024-10-28 10:14:02 +0000

    Changed default from email

--- paradise/settings.py
+++ paradise/settings.py
@@ -225,7 +225,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'paradisepending@example.com'
+    DEFAULT_FROM_EMAIL = 'colinstlouiscs@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
