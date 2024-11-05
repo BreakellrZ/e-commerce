66d45a751cbdaf9ed888879c5c5e654c809b355c
Colin Breakell <cbreakell99@gmail.com>
2024-11-05 19:33:47 +0000

    Feat: Added delete feature to reviews

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create/', views.review_create, name='review_create'),
-
+    path('create/', views.createReview, name='review_create'),
+    path('update/<str:pk>/', views.updateReview, name='review_update'),
+    path('delete/<str:pk>/', views.deleteReview, name='review_delete'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -7,7 +7,7 @@ from .forms import ReviewForm
 
 
 @login_required
-def review_create(request):
+def createReview(request):
 
     reviews = Reviews.objects.order_by('-created_on')[:12]
 
@@ -17,7 +17,8 @@ def review_create(request):
             review = form.save(commit=False)
             review.author = request.user
             review.save()
-
+            messages.success(
+                request, 'Your review has been created pending approval')
     else:
         form = ReviewForm()
 
@@ -25,3 +26,43 @@ def review_create(request):
                'reviews': reviews, }
 
     return render(request, 'reviews/review_form.html', context)
+
+
+@login_required
+def updateReview(request, pk):
+    reviews = Reviews.objects.order_by('-created_on')[:12]
+    review = Reviews.objects.get(id=pk)
+    form = ReviewForm(instance=review)
+    messages.success(
+                request, 'EDIT YOUR REVIEW')
+
+    if request.method == "POST":
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid() and review.author == request.user:
+            review = form.save(commit=False)
+            review.author = request.user
+            review.save()
+            messages.success(
+                request, 'Your review has been updated')
+            return redirect('/')
+        else:
+            messages.error(
+                request, ' ERROR: You must be the orginal author to edit this Review!')
+            return redirect('/')
+
+    context = {'form': form,
+               'reviews': reviews}
+    return render(request,  'reviews/review_form.html', context)
+
+
+@login_required
+def deleteReview(request, pk):
+    review = Reviews.objects.get(id=pk)
+    if request.method == "POST":
+        review.delete()
+        messages.success(
+            request, 'Your review has been deleted')
+        return redirect("/")
+    context = {'item': review}
+    return render(request, 'reviews/review_delete.html', context)
+
